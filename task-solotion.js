5b085000a519ec739177ebfef1f352cf01364762
FARUK2059 <omarfaruk34552@gmail.com>
2025-06-04 19:26:55 +0600

    task 3 solution

--- task-solotion.js
+++ task-solotion.js
@@ -32,3 +32,24 @@ function getBookTitles(booksArray) {
 }
 
 // console.log(getBookTitles(books)); 
+
+
+// 3. Function Composition
+
+function square(num) {
+  return num * num;
+}
+
+function double(num) {
+  return num * 2;
+}
+
+function addFive(num) {
+  return num + 5;
+}
+
+function composeFunctions(num) {
+  return addFive(double(square(num)));
+}
+
+// console.log(composeFunctions(3));
