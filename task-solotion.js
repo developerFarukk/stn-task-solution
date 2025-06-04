fc5fb203ccc6ecd77eb33af4e89c98d8dc542987
FARUK2059 <omarfaruk34552@gmail.com>
2025-06-04 19:24:06 +0600

    task 2 solve

--- task-solotion.js
+++ task-solotion.js
@@ -16,3 +16,19 @@ function filterAndMap(peopleArray) {
 }
 
 // console.log(filterAndMap(people));
+
+
+
+// 2. Object Manipulation
+
+const books = [
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+  { title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+  { title: '1984', author: 'George Orwell', year: 1949 }
+];
+
+function getBookTitles(booksArray) {
+  return booksArray.map(book => book.title);
+}
+
+// console.log(getBookTitles(books)); 
